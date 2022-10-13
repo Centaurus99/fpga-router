8c728b487039ef83a0d7c374d38e4d3b6f9f83e7
Centaurus99 <1102569568@qq.com>
2022-10-13 22:20:28 +0800

    fix some bug

--- tanlabs/tanlabs.srcs/sources_1/new/icmpv6_checksum.sv
+++ tanlabs/tanlabs.srcs/sources_1/new/icmpv6_checksum.sv
@@ -1,29 +1,27 @@
+`timescale 1ns / 1ps `default_nettype none
+
 `include "frame_datapath.vh"
 
-module icmpv6_checksum 
-#(
+module icmpv6_checksum #(
     parameter PACKET_LENGTH = 88,
     parameter MOD = 4'd0
-)
-(
-    input frame_beat beat,
-    output wire [15:0] sum
+) (
+    input  frame_beat        beat,
+    output wire       [15:0] sum
 );
 
+    // 注意这样的代码只能算单包 88 Byte
     reg [23:0] sum_reg = 6'h000000;
     always_comb begin
         // 加上next_header和payload_len
         sum_reg = beat.data.ip6.next_hdr + {beat.data.ip6.payload_len[7:0], beat.data.ip6.payload_len[15:8]};
-        // 注意这样的代码只能算单包 88 Byte
-    end
-    genvar i;
-    generate 
-    for(i = 0;i < PACKET_LENGTH;i= i + 2) begin
-        always_comb begin
-            sum_reg = sum_reg + {beat.data.ip6[8 * (i + 1) - 1:8 * (i)], beat.data.ip6[8 * (i + 2) - 1:8 * (i + 1)]};
+        for (int i = 0; i < PACKET_LENGTH; i = i + 2) begin
+            sum_reg = sum_reg + {beat.data.ip6[8*i+:8], beat.data.ip6[8*(i+1)+:8]};
         end
+        sum_reg = {sum_reg[7:0], sum_reg[15:8]} + sum_reg[23:16];
+        sum_reg = {sum_reg[7:0], sum_reg[15:8]} + sum_reg[23:16];
     end
-    endgenerate
-    assign sum = ~{sum_reg[7:0], sum_reg[15:8]}; // sum_reg仅仅是数值和
+
+    assign sum = ~{sum_reg[7:0], sum_reg[15:8]};  // sum_reg仅仅是数值和
 
 endmodule
