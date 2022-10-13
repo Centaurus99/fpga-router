7a358c9b383aaaacca85f27b5189426d6002ba5c
Centaurus99 <1102569568@qq.com>
2022-10-13 23:28:40 +0800

    finish checksum

--- tanlabs/tanlabs.srcs/sim_1/new/tb_checksum.sv
+++ tanlabs/tanlabs.srcs/sim_1/new/tb_checksum.sv
@@ -11,7 +11,10 @@ module tb_checksum #(
     reg        [15:0] sum;
 
     initial begin
-
+        in = 0;
+        // 一个有合法校验和的包, sum 输出理应为 0
+        in.data = {<<8{688'hffffffffffff544553545f3081006000000000203aff2a0eaa0604970a000000000000000002fe80000000000000020000fffe030a0088003ffa600000002a0eaa0604970a0000000000000000020201544553545f30, 16'b0}};
+        #100;
         $finish;
     end
 
