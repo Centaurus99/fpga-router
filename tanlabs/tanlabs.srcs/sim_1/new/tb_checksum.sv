17b49f525b5d0fd5ca039e9c315d08f11eba08af
Yue Chen <chen-yue20@mails.tsinghua.edu.cn>
2022-10-20 22:14:05 +0800

    new version of checksum

--- tanlabs/tanlabs.srcs/sim_1/new/tb_checksum.sv
+++ tanlabs/tanlabs.srcs/sim_1/new/tb_checksum.sv
@@ -7,17 +7,46 @@ module tb_checksum #(
     parameter MOD = 4'd0
 ) ();
 
+    reg reset;
+    initial begin
+        reset = 0;
+        #1000
+        reset = 1;
+        #1000
+        reset = 0;
+    end
+
+    wire clk_125M;
+
+    clock clock_i(
+        .clk_125M(clk_125M)
+    );
+
     frame_beat        in;
+    frame_beat        out;
     reg        [15:0] sum;
+    reg          in_ready;
+    reg         out_ready = 1;
 
     initial begin
-
+        in = 0;
+        // 涓�涓湁鍚堟硶鏍￠獙鍜岀殑鍖�, sum 杈撳嚭鐞嗗簲涓� 0
+        in.data = {<<8{688'hffffffffffff544553545f3081006000000000203aff2a0eaa0604970a000000000000000002fe80000000000000020000fffe030a0088003ffa600000002a0eaa0604970a0000000000000000020201544553545f30, 16'b0}};
+        in.valid = 1;
+        in.is_first = 1;
+        in.meta.ndp_packet = 1;
+        #100;
         $finish;
     end
 
     icmpv6_checksum dut (
-        .beat(in),
-        .sum (sum)
+        .clk(clk_125M),
+        .reset(reset),
+        .in(in),
+        .in_ready(in_ready),
+        .out(out),
+        .sum(sum),
+        .out_ready(out_ready)
     );
 
 endmodule
--- tanlabs/tanlabs.srcs/sources_1/new/icmpv6_checksum.sv
+++ tanlabs/tanlabs.srcs/sources_1/new/icmpv6_checksum.sv
@@ -6,22 +6,80 @@ module icmpv6_checksum #(
     parameter PACKET_LENGTH = 88,
     parameter MOD = 4'd0
 ) (
-    input  frame_beat        beat,
-    output wire       [15:0] sum
+    input wire clk,
+    input wire reset,
+
+    input  frame_beat in,
+    output reg        in_ready,
+
+    output frame_beat        out,
+    output reg        [15:0] sum,
+    input  wire              out_ready
 );
 
-    // 注意这样的代码只能算单包 88 Byte
-    reg [23:0] sum_reg = 6'h000000;
+    frame_beat s1;
+    int  s1_state;
+    wire s1_ready;
+    assign in_ready = (s1_ready && s1_state == 0) || !in.valid;
+
     always_comb begin
-        // 加上next_header和payload_len
-        sum_reg = beat.data.ip6.next_hdr + {beat.data.ip6.payload_len[7:0], beat.data.ip6.payload_len[15:8]};
-        for (int i = 8; i < beat.ip6.payload_len + 40; i = i + 2) begin
-            sum_reg = sum_reg + {beat.data.ip6[8*i+:8], beat.data.ip6[8*(i+1)+:8]};
-        end
-        sum_reg = {sum_reg[7:0], sum_reg[15:8]} + sum_reg[23:16];
-        sum_reg = {sum_reg[7:0], sum_reg[15:8]} + sum_reg[23:16];
+        s1.valid = s1_state == 0;
     end
 
-    assign sum = ~{sum_reg[7:0], sum_reg[15:8]};  // sum_reg仅仅是数值和
+    // 娉ㄦ剰杩欐牱鐨勪唬鐮佸彧鑳界畻鍗曞寘 88 Byte
+    reg   [8 * 127:0] long_sum_pack = 0;
+    reg   [8 * 127:0] long_sum_pack_copy = 0;
+    reg   [8 * 127:0] long_sum_pack_sum = 0;
+    reg   [31:0] move_reg;
+
+    // always_comb begin
+    //     // 鍔犱笂next_header鍜宲ayload_len
+    //     sum_reg = in.data.ip6.next_hdr;
+    //     sum_reg = sum_reg + {in.data.ip6.payload_len[7:0], in.data.ip6.payload_len[15:8]};
+    //     for (int i = 8; i < 72; i = i + 2) begin
+    //         sum_reg = sum_reg + {in.data.ip6[8*i+:8], in.data.ip6[8*(i+1)+:8]};
+    //     end
+    //     sum_reg = {8'b0, sum_reg[15:0]} + sum_reg[23:16];
+    //     sum_reg = {8'b0, sum_reg[15:0]} + sum_reg[23:16];
+    // end
+
+    always_ff @(posedge clk or posedge reset) begin
+        if (reset) begin
+            s1_state    <= 0;
+        end else begin
+            case (s1_state)
+                0: begin
+                    if (s1_ready) begin
+                        s1 <= in;
+                        move_reg = 8;
+                        long_sum_pack[8 * 72 - 1 : 0] = in.data.ip6[8 * 86 - 1 : 8 * 8];
+                        long_sum_pack[8 * 73 - 1 : 8 * 72] = {8'h00, in.data.ip6.next_hdr};
+                        long_sum_pack[8 * 74 - 1 : 8 * 73] = {in.data.ip6.payload_len[7:0], in.data.ip6.payload_len[15:8]};
+                        if (in.valid && in.is_first && !in.meta.drop && in.meta.ndp_packet) begin
+                            s1_state <= 10;
+                        end
+                    end
+                end
+                7: begin
+                    out.data.ip6.p[31 : 16] <= long_sum_pack[15 : 0]; // sum_reg浠呬粎鏄暟鍊煎拰
+                    s1_state <= 0;
+                end
+                default: begin
+                    long_sum_pack_copy <= long_sum_pack >>> move_reg;
+                    for(int i = 0; i < 128; i ++) begin
+                        long_sum_pack_sum[(8 * i)+ : 8] <= long_sum_pack_copy[(8 * i)+ : 8] + long_sum_pack[(8 * i)+ : 8];
+                        if({8'b0, long_sum_pack_sum[(8 * i)+ : 8]} < {8'b0, long_sum_pack_copy[(8 * i)+:8]} + {8'b0, long_sum_pack[(8 * i)+:8]}) begin
+                            long_sum_pack_sum[(8 * i)+ : 8] <= long_sum_pack_sum[(8 * i)+ : 8] + 1;
+                        end
+                    end
+                    move_reg <= move_reg * 2;
+                    s1_state <= s1_state + 1;
+                end
+            endcase
+        end
+    end
+
+    assign out      = s1;
+    assign s1_ready = out_ready;
 
 endmodule
