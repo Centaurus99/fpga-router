26d649ba94f2316c94132a59a06b4556a58c673c
Yue Chen <chen-yue20@mails.tsinghua.edu.cn>
2022-10-28 13:53:24 +0800

    first structure

--- tanlabs/tanlabs.srcs/sources_1/new/forwarding_ram_controller.sv
+++ tanlabs/tanlabs.srcs/sources_1/new/forwarding_ram_controller.sv
@@ -42,4 +42,61 @@ module forwarding_ram_controller #(
 );
     // TODO: controller
 
+    typedef enum { 
+        ST_INIT,
+        ST_READ_BRAM1,
+        ST_READ_BRAM2,
+        ST_READ_BRAM3,
+        ST_READ_BRAM4,
+        ST_READ_LEAF1,
+        ST_READ_LEAF2,
+        ST_READ_HOP1,
+        ST_READ_HOP2,
+        ST_WRITE_BRAM1,
+        ST_WRITE_BRAM2,
+        ST_WRITE_BRAM3,
+        ST_WRITE_BRAM4,
+        ST_WRITE_LEAF1,
+        ST_WRITE_LEAF2,
+        ST_WRITE_HOP1,
+        ST_WRITE_HOP2,
+    } state_slave;
+
+    state_slave state = ST_INIT;
+    reg wb_stb_i_reg = 1'b0;
+
+    always_ff(posedge clk_i) begin
+        if(rst_i) begin
+            wb_ack_o <= 1'b0;
+            wb_dat_o <= 1'b0;
+        end else begin
+            case(state)
+                ST_INIT:
+                    if(wb_stb_i == 1'b1 && wb_stb_i_reg = 1'b0 && wb_cyc_i == 1'b1 && wb_cyc_i == 1'b0) begin
+                        wb_stb_i_reg <= wb_stb_i;
+                        wb_cyc_i_reg <= wb_cyc_i;
+                        if(wb_we_i == 1'b0) begin
+                            if(wb_adr_i >= 32'h40000000 && wb_adr_i < 32'h48000000)
+                                state <= ST_READ_BRAM1
+                            if(wb_adr_i >= 32'h50000000 && wb_adr_i < 32'h51000000)
+                                state <= ST_READ_LEAF1
+                            if(wb_adr_i >= 32'h51000000 && wb_adr_i < 32'h52000000)
+                                state <= ST_READ_HOP1
+                        end else begin
+                            if(wb_adr_i >= 32'h40000000 && wb_adr_i < 32'h48000000)
+                                state <= ST_WRITE_BRAM1
+                            if(wb_adr_i >= 32'h50000000 && wb_adr_i < 32'h51000000)
+                                state <= ST_WRITE_LEAF1
+                            if(wb_adr_i >= 32'h51000000 && wb_adr_i < 32'h52000000)
+                                state <= ST_WRITE_HOP1
+                        end
+                        wb_ack_o = 1'b1;
+                    end else begin
+                        wb_ack_o = 1'b1;
+                    end
+                ST_READ_BRAM1:
+            endcase 
+        end
+    end
+
 endmodule
