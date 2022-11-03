7258df1a4bbacbc5555df504eedc6cb613c4574d
Yue Chen <chen-yue20@mails.tsinghua.edu.cn>
2022-11-03 23:08:12 +0800

    change push

--- tanlabs/tanlabs.srcs/sources_1/new/forwarding_ram_controller.sv
+++ tanlabs/tanlabs.srcs/sources_1/new/forwarding_ram_controller.sv
@@ -59,7 +59,7 @@ module forwarding_ram_controller #(
     always_comb begin
         if (wb_adr_i[WISHBONE_ADDR_WIDTH-1:WISHBONE_ADDR_WIDTH-4] == 4'h4) begin
             ft_addr[wb_adr_i[WISHBONE_ADDR_WIDTH-5:WISHBONE_ADDR_WIDTH-8] + 1][CHILD_ADDR_WIDTH - 1:0] = {4'b0, wb_adr_i[WISHBONE_ADDR_WIDTH-9:4]};
-            inner_place[4:0] = {1'b0, wb_adr_i[3:0]}
+            inner_place[4:0] = {1'b0, wb_adr_i[3:0]};
             storetype = BRAM;
             ft_en_reg = 1'b1;
             ft_we_reg = 1'b0;
@@ -79,31 +79,31 @@ module forwarding_ram_controller #(
         ft_we_reg = 1'b0;
     end
 
-    always_comb begin
-        case (storetype)
-            BRAM:
-                ft_din = ft_dout;
-                case (inner_place[3:2])
-                    2'b00:
-                        ft_din.child_map[CHILD_MAP_SIZE-1:0] = wb_dat_i[CHILD_MAP_SIZE-1:0];
-                    2'b01:
-                        ft_din.leaf_map[LEAF_MAP_SIZE-1:0] = wb_dat_i[LEAF_MAP_SIZE-1:0];
-                    2'b10:
-                        ft_din.child_base_addr[CHILD_ADDR_WIDTH - 1:0] = wb_dat_i[CHILD_ADDR_WIDTH - 1:0];
-                    2'b11:
-                        ft_din.leaf_base_addr[CHILD_ADDR_WIDTH - 1:0] = wb_dat_i[LEAF_ADDR_WIDTH - 1:0];
-                endcase 
-            LEAF:
-                leaf_in[NEXT_HOP_ADDR_WIDTH - 1:0] = wb_dat_i[NEXT_HOP_ADDR_WIDTH - 1:0];
-            NEXTHOP:
-                if(inner_place[4] == 1'b1) begin
-                    // 这里应该是port，理论上后面应该全是零，
-                    next_hop_in.port[7:0] = wb_dat_o[7:0]
-                end else begin
-                    next_hop_out.ip[8*(inner_place[3:0])+:32] = wb_dat_i[WISHBONE_DATA_WIDTH-1:0];
-                end
-        endcase
-    end
+    // always_comb begin
+    //     case (storetype)
+    //         BRAM:
+    //             ft_din = ft_dout;
+    //             case (inner_place[3:2])
+    //                 2'b00:
+    //                     ft_din.child_map[CHILD_MAP_SIZE-1:0] = wb_dat_i[CHILD_MAP_SIZE-1:0];
+    //                 2'b01:
+    //                     ft_din.leaf_map[LEAF_MAP_SIZE-1:0] = wb_dat_i[LEAF_MAP_SIZE-1:0];
+    //                 2'b10:
+    //                     ft_din.child_base_addr[CHILD_ADDR_WIDTH - 1:0] = wb_dat_i[CHILD_ADDR_WIDTH - 1:0];
+    //                 2'b11:
+    //                     ft_din.leaf_base_addr[CHILD_ADDR_WIDTH - 1:0] = wb_dat_i[LEAF_ADDR_WIDTH - 1:0];
+    //             endcase 
+    //         LEAF:
+    //             leaf_in[NEXT_HOP_ADDR_WIDTH - 1:0] = wb_dat_i[NEXT_HOP_ADDR_WIDTH - 1:0];
+    //         NEXTHOP:
+    //             if(inner_place[4] == 1'b1) begin
+    //                 // 这里应该是port，理论上后面应该全是零，
+    //                 next_hop_in.port[7:0] = wb_dat_o[7:0];
+    //             end else begin
+    //                 next_hop_out.ip[8*(inner_place[3:0])+:32] = wb_dat_i[WISHBONE_DATA_WIDTH-1:0];
+    //             end
+    //     endcase
+    // end
 
     typedef enum {
         ST_IDLE,
@@ -111,7 +111,7 @@ module forwarding_ram_controller #(
         ST_DONE
     } state_slave;
 
-    state_slave state = ST_INIT;
+    state_slave state = ST_IDLE;
 
     always_ff @(posedge clk_i) begin
         if (rst_i) begin
@@ -119,7 +119,7 @@ module forwarding_ram_controller #(
             wb_dat_o <= 1'b0;
         end else begin
             case (state)
-                ST_IDLE:
+                ST_IDLE: begin
                     if (wb_stb_i == 1'b1 && wb_cyc_i == 1'b1) begin
                         wb_adr_i_reg <= wb_adr_i;
                         if(wb_we_i == 1'b0) begin
@@ -130,42 +130,52 @@ module forwarding_ram_controller #(
                         wb_ack_o <= 1'b0;
                     end
                     wb_ack_o <= 1'b0;
-                ST_DONE:
+                end
+                ST_DONE: begin
                     if(wb_we_i == 1'b0) begin
                         case (storetype)
-                            BRAM:
+                            BRAM: begin
                                 case (inner_place[3:2])
-                                    2'b00:
-                                        wb_dat_o[WISHBONE_DATA_WIDTH-1:0] <= {ft_dout.child_map[CHILD_MAP_SIZE - 1:0], 16'b0};
-                                    2'b01:
-                                        wb_dat_o[WISHBONE_DATA_WIDTH-1:0] <= {ft_dout.leaf_map[LEAF_MAP_SIZE - 1:0], 16'b0};
-                                    2'b10:
-                                        wb_dat_o[WISHBONE_DATA_WIDTH-1:0] <= {ft_dout.child_base_addr[CHILD_ADDR_WIDTH - 1:0], 8'b0};
-                                    2'b11:
-                                        wb_dat_o[WISHBONE_DATA_WIDTH-1:0] <= {ft_dout.leaf_base_addr[LEAF_ADDR_WIDTH - 1:0], 16'b0};
+                                    2'b00: begin
+                                        wb_dat_o[WISHBONE_DATA_WIDTH-1:0] <= {16'b0, ft_dout.child_map[15:0]};
+                                    end
+                                    2'b01: begin
+                                        wb_dat_o[WISHBONE_DATA_WIDTH-1:0] = {ft_dout.leaf_map[LEAF_MAP_SIZE - 1:0], 16'b0};
+                                    end
+                                    2'b10: begin
+                                        wb_dat_o[WISHBONE_DATA_WIDTH-1:0] = {ft_dout.child_base_addr[CHILD_ADDR_WIDTH - 1:0], 8'b0};
+                                    end
+                                    2'b11: begin
+                                        wb_dat_o[WISHBONE_DATA_WIDTH-1:0] = {ft_dout.leaf_base_addr[LEAF_ADDR_WIDTH - 1:0], 16'b0};
+                                    end
                                 endcase 
-                            LEAF:
-                                wb_dat_o[WISHBONE_DATA_WIDTH-1:0] <= {leaf_out.next_hop_addr[NEXT_HOP_ADDR_WIDTH - 1:0], 24'b0}
-                            NEXTHOP:
+                            end
+                            LEAF: begin
+                                wb_dat_o[WISHBONE_DATA_WIDTH-1:0] = {leaf_out.next_hop_addr[NEXT_HOP_ADDR_WIDTH - 1:0], 24'b0};
+                            end
+                            NEXTHOP: begin
                                 if(inner_place[4] == 1'b1) begin
                                     // 这里应该是port，理论上后面应该全是零，
-                                    wb_dat_o[WISHBONE_DATA_WIDTH-1:0] <= {next_hop_out.port[7:0], 24'b0};
+                                    wb_dat_o[WISHBONE_DATA_WIDTH-1:0] = {24'b0, next_hop_out.port[7:0]};
                                 end else begin
-                                    wb_dat_o[WISHBONE_DATA_WIDTH-1:0] <= next_hop_out.ip[8*(inner_place[3:0])+:32];
+                                    wb_dat_o[WISHBONE_DATA_WIDTH-1:0] = next_hop_out.ip[8*(inner_place[3:0])+:32];
                                 end
+                            end
                         endcase 
                         state <= ST_IDLE;
                     end else begin 
                         state <= ST_IDLE;
                     end
                     wb_ack_o <= 1'b1;
-                ST_WRITE:
+                end
+                ST_WRITE: begin
                     if(wb_we_i == 1'b0) begin
                         case (storetype)
-                            BRAM:
+                            BRAM: begin
                                 case (inner_place[3:2])
-                                    2'b00:
-                                         = {ft_dout.child_map[CHILD_MAP_SIZE - 1:0], 16'b0};
+                                    2'b00: begin
+                                        wb_dat_o[WISHBONE_DATA_WIDTH-1:0] = {16'b0, ft_dout.child_map[15:0]};
+                                    end
                                     2'b01:
                                         wb_dat_o[WISHBONE_DATA_WIDTH-1:0] = {ft_dout.leaf_map[LEAF_MAP_SIZE - 1:0], 16'b0};
                                     2'b10:
@@ -173,15 +183,18 @@ module forwarding_ram_controller #(
                                     2'b11:
                                         wb_dat_o[WISHBONE_DATA_WIDTH-1:0] = {ft_dout.leaf_base_addr[LEAF_ADDR_WIDTH - 1:0], 16'b0};
                                 endcase 
-                            LEAF:
-                                wb_dat_o[WISHBONE_DATA_WIDTH-1:0] = {leaf_out.next_hop_addr[NEXT_HOP_ADDR_WIDTH - 1:0], 24'b0}
-                            NEXTHOP:
+                            end
+                            LEAF: begin
+                                wb_dat_o[WISHBONE_DATA_WIDTH-1:0] = {leaf_out.next_hop_addr[NEXT_HOP_ADDR_WIDTH - 1:0], 24'b0};
+                            end
+                            NEXTHOP: begin
                                 if(inner_place[4] == 1'b1) begin
                                     // 这里应该是port，理论上后面应该全是零，
                                     wb_dat_o[WISHBONE_DATA_WIDTH-1:0] = {next_hop_out.port[7:0], 24'b0};
                                 end else begin
                                     wb_dat_o[WISHBONE_DATA_WIDTH-1:0] = next_hop_out.ip[8*(inner_place[3:0])+:32];
                                 end
+                            end
                         endcase 
                         state <= ST_DONE;
                     end else begin 
@@ -189,15 +202,14 @@ module forwarding_ram_controller #(
                         state <= ST_IDLE;
                     end
 
-
-                default:
+                end
+                default: begin
                     // 理论上不能来这里
                     state <= ST_IDLE;
+                end
             endcase
         end
     end
 
-    assign ft_en = ft_en_reg;
-    assign ft_we = ft_we_reg;
 
 endmodule
