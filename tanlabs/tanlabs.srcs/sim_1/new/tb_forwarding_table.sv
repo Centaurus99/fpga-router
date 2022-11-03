045e32f563b5ae069842dabcf41835ae4df6aecb
Centaurus99 <1102569568@qq.com>
2022-11-04 00:17:21 +0800

    add mem.txt in forwarding testbench

--- tanlabs/tanlabs.srcs/sim_1/new/tb_forwarding_table.sv
+++ tanlabs/tanlabs.srcs/sim_1/new/tb_forwarding_table.sv
@@ -25,19 +25,21 @@ module tb_forwarding_table #(
     assign forwarded_ready = 1'b1;
 
     logic [127:0] const_ip;
-    assign const_ip = {<<8{128'h2a0e_aa06_0497_0a00_0000_0000_1234_5678}};
+    assign const_ip = {<<8{128'h2a0e_aa06_0497_0a01_0000_0000_1234_5678}};
 
     wire                             cpu_clk;
-    wire                             wb_cyc_i;
-    wire                             wb_stb_i;
+    reg                              wb_cyc_i;
+    reg                              wb_stb_i;
     reg                              wb_ack_o;
-    wire [  WISHBONE_ADDR_WIDTH-1:0] wb_adr_i;
-    wire [  WISHBONE_DATA_WIDTH-1:0] wb_dat_i;
+    reg  [  WISHBONE_ADDR_WIDTH-1:0] wb_adr_i;
+    reg  [  WISHBONE_DATA_WIDTH-1:0] wb_dat_i;
     reg  [  WISHBONE_DATA_WIDTH-1:0] wb_dat_o;
-    wire [WISHBONE_DATA_WIDTH/8-1:0] wb_sel_i;
-    wire                             wb_we_i;
+    reg  [WISHBONE_DATA_WIDTH/8-1:0] wb_sel_i;
+    reg                              wb_we_i;
 
+    int                              fd;
     initial begin
+        fd    = $fopen("mem.txt", "r");
         reset = 1;
         #20;
         reset = 0;
@@ -47,29 +49,61 @@ module tb_forwarding_table #(
     end
 
     typedef enum {
-        ST_INIT,
-        ST_SENT
+        ST_WRITE_RAM,
+        ST_SENT,
+        ST_SENT_WAIT,
+        ST_DONE
     } state_t;
 
-    state_t state;
+    state_t                           state;
 
-    always_ff @(posedge clk_125M) begin
+    int                               write_count;
+    int                               ret;
+    reg     [WISHBONE_DATA_WIDTH-1:0] ram_addr;
+    reg     [WISHBONE_DATA_WIDTH-1:0] ram_data;
+
+    always_ff @(posedge clk_100M or posedge reset) begin
         if (reset) begin
-            state <= ST_INIT;
-            in    <= '{default: 0};
+            state       <= ST_WRITE_RAM;
+            in          <= '{default: 0};
+            write_count <= 0;
         end else begin
             case (state)
-                ST_INIT: begin
+                ST_WRITE_RAM: begin
+                    if (write_count == 0 || wb_ack_o) begin
+                        write_count <= write_count + 1;
+                        ret = $fscanf(fd, "%x%x", ram_addr, ram_data);
+                        if (ret != 2) begin
+                            wb_cyc_i <= 1'b0;
+                            wb_stb_i <= 1'b0;
+                            state    <= ST_SENT;
+                        end else begin
+                            wb_cyc_i <= 1'b1;
+                            wb_stb_i <= 1'b1;
+                            wb_we_i  <= 1'b1;
+                            wb_sel_i <= 4'b1111;
+                            wb_adr_i <= ram_addr;
+                            wb_dat_i <= ram_data;
+                            state    <= ST_WRITE_RAM;
+                        end
+                    end
+                end
+                ST_SENT: begin
                     in.valid        <= 1;
                     in.is_first     <= 1;
                     in.data.ip6.dst <= const_ip;
                     state           <= ST_SENT;
                 end
-                ST_SENT: begin
+                ST_SENT_WAIT: begin
                     if (in_ready) begin
                         in    <= '{default: 0};
-                        state <= ST_INIT;
+                        state <= ST_DONE;
                     end
+                end
+                ST_DONE: begin
+                    state <= ST_DONE;
+                    #100;
+                    $finish;
                 end
             endcase
         end
