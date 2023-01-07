e20767f895284ab0a152b45501c3bb2b223a11e8
Yue Chen <chen-yue20@mails.tsinghua.edu.cn>
2023-01-07 16:35:49 +0800

    a few new code

--- firmware/include/ripng.h
+++ firmware/include/ripng.h
@@ -25,8 +25,13 @@ void send_ripngentries();
 
 #define RIPNGPORT 0x0209 // 521
 
-#define RipngEntryNum(len) (((len) - sizeof(EtherHeader) - sizeof(IP6Header) - sizeof(UDPHeader) - sizeof(RipngHead)) / sizeof(RipngEntry))
-#define RipngHead_PTR(packet) (RipngHead *)((packet) + sizeof(EtherHeader) + sizeof(IP6Header) + sizeof(UDPHeader)))
-#define RipngEntries_PTR(packet) (RipngEntry *)((packet) + sizeof(EtherHeader) + sizeof(IP6Header) + sizeof(UDPHeader) + sizeof(RipngHead)))
+#define RipngEntryNum(len) (uint32_t)(((len) - sizeof(EtherHeader) - sizeof(IP6Header) - sizeof(UDPHeader) - sizeof(RipngHead)) / sizeof(RipngEntry))
+#define RipngHead_PTR(packet) (RipngHead *)((packet) + sizeof(EtherHeader) + sizeof(IP6Header) + sizeof(UDPHeader))
+#define RipngEntries_PTR(packet) (RipngEntry *)((packet) + sizeof(EtherHeader) + sizeof(IP6Header) + sizeof(UDPHeader) + sizeof(RipngHead))
+
+#define RIPNG_REQUEST 0x1
+#define RIPNG_RESPONSE 0x2
+
+#define METRIC_INF 0x0f
 
 #endif
--- firmware/ripng.c
+++ firmware/ripng.c
@@ -1,5 +1,31 @@
 #include <ripng.h>
+#include <header.h>
 #include <dma.h>
+
+const RipngEntry request_for_all = {
+    .addr = 0, .route_tag = 0x0000, .prefix_len = 0x00,  .metric = 0x0f
+};
+
+void _ripng(uint8_t *packet, uint32_t length) {
+    // 此处处理 RIPNG 协议
+    RipngHead *riphead = RipngHead_PTR(packet);
+    uint32_t ripng_num = RipngEntryNum(length);
+    if(ripng_num * sizeof(RipngEntry) + sizeof(RipngHead) + sizeof(UDPHeader) + sizeof(IP6Header) + sizeof(EtherHeader) == length) { 
+        RipngEntry *ripentry = RipngEntries_PTR(packet);
+        if(riphead->command == RIPNG_REQUEST) {
+            uint32_t ripng_num = RipngEntryNum(length);
+            for(uint32_t i = 0; i < ripng_num; i++) {
+                if(in6_addr_equal(ripentry[i].addr, request_for_all.addr) && ripentry[i].metric == request_for_all.metric && ripentry[i].prefix_len == request_for_all.prefix_len) {
+                    // TODO: send all of your route tables
+                }
+            }
+        } else if(riphead->command == RIPNG_RESPONSE) {
+            
+        }
+    } else {
+        // 不合法的 ripng 包
+    }
+}
 
 void receive_ripngentry(RipngEntry* entry) {
     if(entry->metric == 0xff) {
@@ -8,11 +34,6 @@ void receive_ripngentry(RipngEntry* entry) {
     } else {
     
     }
-}
-
-void _ripng(uint8_t *packet, uint32_t length) {
-    // 此处处理RIPNG协议
-    
 }
 
 void send_ripngentries() {
